ffdc57669dfc8f29279af809ce820e071144a6f9
Eugene <yaheni@amazon.com>
2019-09-25 13:59:58 -0700

    HW4: setup

--- ps/ps3/run.sh
+++ ps/ps3/run.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-cd /home/ubuntu/repo/cs344/ps/ps4/
-rm HW4_
-mv HW4 HW4_
+cd /home/ubuntu/repo/cs344/ps/ps3/
+rm HW3_
+mv HW3 HW3_
 make
-./HW4 red_eye_effect_5.jpg red_eye_effect_template_5.jpg ../../out.jpg
-ls
-cp ./red_eye_effect.gold ../../out2.jpg
+./HW3 memorial_raw.png
+mv HW3_output.png          ./out.jpg
+cp memorial_png.gold ./out2.jpg
+cp out.jpg  ../../
+cp out2.jpg ../../
 
 # scp ubuntu@$CIP:"/home/ubuntu/repo/cs344/ps/ps1/HW2_output.png" ~/repo/cs344/
 # scp ubuntu@$CIP:"/home/ubuntu/repo/cs344/ps/ps1/HW2_differenceImage.png" ~/repo/cs344/
